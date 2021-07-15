abe1c32604580ecb51c9f00e1ad8f2af753c6d00
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-15 10:45:47 -0500

    Finished AXIOS vs Fetch code.

--- script.js
+++ script.js
@@ -23,47 +23,93 @@
 //     console.log(err)
 //   })
 
-//Chaining Fetch
-const checkStatusAndParse = (response) => {
-  if (!response.ok) throw new Error(`Status Code Error: ${response.status}`)
+//Fetch Chained Requests and pulling multiple pages of data.
+// const checkStatusAndParse = (response) => {
+//   if (!response.ok) throw new Error(`Status Code Error: ${response.status}`)
 
-  return response.json()
-}
+//   return response.json()
+// }
 
-const printPlanets = (data) => {
-  console.log('Loaded 10 more planets...')
-  for (let planet of data.results) {
-    console.log(planet.name)
-  }
-  return Promise.resolve(data.next)
-}
+// const printPlanets = (data) => {
+//   console.log("Loaded 10 more planets...")
+//   for (let planet of data.results) {
+//     console.log(planet.name)
+//   }
+//   return Promise.resolve(data.next)
+// }
 
-const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
-  return fetch(url)
-}
-
-fetchNextPlanets()
-  .then(checkStatusAndParse)
-  .then(printPlanets)
-  .then(fetchNextPlanets)
-  .then(checkStatusAndParse)
-  .then(printPlanets)
-  .then(fetchNextPlanets)
-  .then(checkStatusAndParse)
-.then(printPlanets)
+// const fetchNextPlanets = (url = "https://swapi.dev/api/planets/") => {
+//   return fetch(url)
+// }
+// //each one of these functions returns a Promise
+// fetchNextPlanets()
+//   .then(checkStatusAndParse)
+//   .then(printPlanets)
+//   .then(fetchNextPlanets)
+//   .then(checkStatusAndParse)
+//   .then(printPlanets)
+//   .then(fetchNextPlanets)
+//   .then(checkStatusAndParse)
+//   .then(printPlanets)
 
 /*AXIOS LIBRARY - An external library for making HTTP requests.*/
 /*We have to include it in our project via script or npm*/
 /*From here on out in the course we will be using AXIOS not Fetch */
 /*AXIOS simplifies the process of making requests*/
 /*AXIOS is using Fetch behind the scenes*/
-/*Key differences is that everything is already parsed for us, no more parsing JSON and .catch works, all non 200 status codes actually get caught*/
+/*Key differences is that everything is already parsed for us, no more parsing JSON from a stream and .catch works, all non 200 status codes actually get caught*/
 
 // axios
 //   .get("https://swapi.dev/api/planets")
 //   .then((response) => {
-//     console.log(response.data.results[0])
+//     console.log(response.data.results)
 //   })
 //   .catch((err) => {
 //     console.log(err)
 //   })
+
+/* AXIOS Chained Requests and pulling multiple pages of data. (refactoring Fetch code above)*/
+
+// axios
+//   .get("https://swapi.dev/api/planets")
+//   .then((response) => {
+//     console.log(response.data)
+//     for (let planet of response.data.results) {
+//       console.log(planet.name)
+//     }
+//     return axios.get(response.data.next)
+//   })
+//   .then((response) => {
+//     console.log(response.data)
+//     for (let planet of response.data.results) {
+//       console.log(planet.name)
+//     }
+//   })
+//   .catch((err) => {
+//   console.log('Error!', err)
+//   })
+
+//above refactored
+const fetchNextPlanets = (url = "https://swapi.dev/api/planets") => {
+  console.log(url)
+  return axios.get(url)
+}
+const printPlanets = ({data}) => {
+  console.log(data)
+  for (let planet of data.results) {
+    console.log(planet.name)
+  }
+  return Promise.resolve(data.next)
+}
+
+fetchNextPlanets()
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .catch((err) => {
+    console.log("Error!", err)
+  })
